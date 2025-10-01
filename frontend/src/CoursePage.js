5d3bc0375106dad1c4b4e7722f4969aeae74c62d
Oliver Persson <oliver.persson.8578@student.lu.se>
2025-10-01 20:35:02 +0200

    Implemented skeleton Reviews component

--- frontend/src/CoursePage.js
+++ frontend/src/CoursePage.js
@@ -1,5 +1,7 @@
 import './CoursePage.css'
 import StarRating from './components/StarRating'
+import like from './img/like.png'
+import dislike from './img/thumbs_down.png'
 
 function CourseHeader() {
   const averageRating = 4.3
@@ -27,19 +29,70 @@ function CourseHeader() {
 function CourseBody() {
   return (
     <div className='course-body-container'>
-      <div className='course-body-reviews'>
+      <div className='course-body-reviews-container'>
         <h1>Reviews</h1>
-        <div className='course-body-reviews-list'>
-          <div className='course-review-card'></div>
+        <div className='course-reviews-list-container'>
+          <ul className='course-reviews-list'>
+            <CourseReview 
+              rating={5} author="Oliver P." title="Great course!" 
+              likesCount={10} dislikesCount={2}
+            />
+            <CourseReview 
+              rating={4} author="Anonymous" title="Very informative" 
+              likesCount={5} dislikesCount={1}
+            />
+            <CourseReview 
+              rating={5} author="Jonathan S." title="Loved it!" 
+              likesCount={8} dislikesCount={0}
+            />
+            <CourseReview 
+              rating={4} author="Anonymous" title="Well structured" 
+              likesCount={6} dislikesCount={1}
+            />
+            <CourseReview 
+              rating={3} author="Anonymous" title="Could be better" 
+              likesCount={3} dislikesCount={4}
+            />
+          </ul>
         </div>
       </div>
-      <div className='course-body-discussions'>
+      <div className='course-body-discussions-container'>
         <h1>Discussions</h1>
         <div className='course-body-discussions-list'>
           <div className='course-discussion-card'></div>
         </div>
       </div>
     </div>
+  )
+}
+
+function CourseReview({ rating, author, title, likesCount, dislikesCount }) {
+  return (
+    <li className='course-review-card'>
+      <div className='course-review-rating'>
+        <StarRating rating={rating} />
+        <span className='course-review-author'>{author}</span>
+      </div>
+      <h3 className='course-review-title'>{title}</h3>
+      <p className='course-review-text'>{`Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. Sed do eiusmod tempor incididunt 
+        ut labore et dolore magna aliqua.`}
+      </p>
+      <div className='course-review-footer'>
+        <div className='course-review-like-field'>
+          <button className='course-review-like-button'>
+            <img className='like-icon' src={like} alt='Like' />
+          </button>
+          <span className='course-review-like-count'>{likesCount}</span>
+        </div>
+        {/*<div className='course-review-like-field'>
+          <button className='course-review-like-button'>
+            <img className='like-icon' src={dislike} alt='Dislike' />
+          </button>
+          <span className='course-review-like-count'>{dislikesCount}</span>
+        </div>*/}
+      </div>
+    </li>
   )
 }
 
