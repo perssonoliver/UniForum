d487f0c65b31aa8432041e6f0a1933b2e5466352
Oliver Persson <oliver.persson.8578@student.lu.se>
2025-10-29 14:59:56 +0100

    Minor changes

--- frontend/src/SearchPage.js
+++ frontend/src/SearchPage.js
@@ -1,11 +1,15 @@
 import './SearchPage.css'
 import SearchForm from './components/SearchForm'
+import logoIcon from './img/logo.png'
 
 function SearchPage() {
   return (
     <div className='search-page-container'>
       <div className='search-content'>
-        <h1 className='search-title'>Welcome to UniForum!</h1>
+        <div className='search-header'>
+          <h1 className='search-title'>Welcome to UniForum!</h1>
+          <img className='search-logo-icon' src={logoIcon} alt='Logo' />
+        </div>
         <SearchForm />
       </div>
     </div>
